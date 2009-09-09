df55e9205da01954cec080db483766798af96a63
Ari Lerner <arilerner@mac.com>
2009-09-08 22:30:14 -0700

    Updating scripts

--- scripts/git-bare-setup.sh
+++ scripts/git-bare-setup.sh
@@ -4,9 +4,26 @@ if [[ -z $1 ]]; then
   echo "You must pass the name of the git repos when using this script";
   echo ""
   echo "Usage: "
-  echo " $0 <nameofrepos>"
+  echo " $0 <nameofrepos> <run?>"
   echo ""
   exit 1;
 fi
 APP_NAME=$1
+RUN=$2
 cd `dirname $APP_NAME`
+
+echo "mkdir $APP_NAME"
+if [[ -z $RUN ]]; then
+  mkdir $APP_NAME
+fi
+
+echo "cd $APP_NAME && git --bare init"
+if [[ -z $RUN ]]; then
+  cd $APP_NAME && git --bare init
+fi
+
+echo "Making post-commit hook"
+echo "cat scripts/git-commit-hook.sh > hooks/post-commit"
+if [[ -z $RUN ]]; then
+  cat scripts/git-commit-hook.sh > $APP_NAME/hooks/post-commit
+fi
