4c4f5192ca803f416d638f4aa90dfb6205fcc28b
Ari Lerner <arilerner@mac.com>
2009-09-08 23:04:25 -0700

    Finishing post commit hook script

--- scripts/git-bare-setup.sh
+++ scripts/git-bare-setup.sh
@@ -4,31 +4,61 @@ if [[ -z $1 ]]; then
   echo "You must pass the name of the git repos when using this script";
   echo ""
   echo "Usage: "
-  echo " $0 <nameofrepos> <run?>"
+  echo " $0 <nameofrepos> <storage directory> <mount location>"
+  echo " Example:"
+  echo "  git-bare-setup.sh test_app /home/auser/repos /mnt"
+  echo ""
+  echo "  This will create a post commit that mounts the latest directory in the mount location"
+  echo "  under the storage directory in squashfs"
   echo ""
   exit 1;
 fi
 APP_NAME=$1
-RUN=$2
+STORE_DIRECTORY=$2
+MOUNT_LOCATION=$3
+FS_DIRECTORY=$STORE_DIRECTORY/$APP_NAME
+
+if [[ -z $STORE_DIRECTORY ]]; then
+  STORE_DIRECTORY="/var/www"
+fi
+
+if [[ -z $MOUNT_LOCATION ]]; then
+  MOUNT_LOCATION="/mnt"
+fi
+
 cd `dirname $APP_NAME`
 
 echo "mkdir $APP_NAME"
-if [[ -z $RUN ]]; then
-  mkdir $APP_NAME
-fi
+mkdir $APP_NAME
 
 echo "cd $APP_NAME && git --bare init"
-if [[ -z $RUN ]]; then
-  cd $APP_NAME && git --bare init
-fi
+cd $APP_NAME && git --bare init
 
 echo "Making post-commit hook"
-if [[ -z $RUN ]]; then  
-  STR="
-    #!/bin/sh
-    
-    Post commit hook
-  "
-  
-   echo $STR > $APP_NAME/hooks/post-commit
+STR="#!/bin/sh
+
+# Bash script from beehive (http://github.com/auser/beehive.git)
+# Runs after a commit and stores sqsh files in $STORE_DIRECTORY
+
+TIMESTAMPED_NAME=$APP_NAME-\$(date +%s).sqsh
+if [[ ! -d \"$FS_DIRECTORY\" ]]; then
+  mkdir -p $FS_DIRECTORY
 fi
+
+# Make the squashfs filesystem
+sudo mksquashfs \$GIT_DIR $FS_DIRECTORY/\$TIMESTAMPED_NAME
+
+if [[ ! -d \"$MOUNT_LOCATION/$APP_NAME\" ]]; then
+  sudo mkdir -p $MOUNT_LOCATION/$APP_NAME
+fi
+
+# Unmount the old one
+if [[ ! -d \"$MOUNT_LOCATION/$APP_NAME\" ]]; then
+  sudo umount $MOUNT_LOCATION/$APP_NAME
+fi
+
+# Mount the new one!
+sudo mount $FS_DIRECTORY/\$TIMESTAMPED_NAME $MOUNT_LOCATION/$APP_NAME -t squashfs -o loop
+"
+
+echo "$STR" > hooks/post-commit
