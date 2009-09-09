8e61dea050875a8deb5a52ad87d4e9be026e67c7
Ari Lerner <arilerner@mac.com>
2009-09-09 01:04:44 -0700

    Fixed git bare setup script

--- scripts/git-bare-setup.sh
+++ scripts/git-bare-setup.sh
@@ -40,26 +40,39 @@ STR="#!/bin/sh
 # Bash script from beehive (http://github.com/auser/beehive.git)
 # Runs after a commit and stores sqsh files in $STORE_DIRECTORY
 
-TIMESTAMPED_NAME=$APP_NAME-\$(date +%s).sqsh
+DATE=\$(date +%s)
+TIMESTAMPED_NAME=$APP_NAME-\$DATE.sqsh
+GIT_REPOS=\$(pwd)
+TMP_GIT_CLONE=/tmp/$APP_NAME
+LOG_FILE=/tmp/$APP_NAME.log
+LOOP_DEVICE=/dev/loop\$DATE
+
+echo $(pwd)
 if [ ! -d \"$FS_DIRECTORY\" ]; then
   mkdir -p $FS_DIRECTORY
 fi
 
+# Checkout the git repos
+git clone \$GIT_REPOS \$TMP_GIT_CLONE
+
 # Make the squashfs filesystem
-sudo mksquashfs \$GIT_DIR $FS_DIRECTORY/\$TIMESTAMPED_NAME
+mksquashfs \$TMP_GIT_CLONE $FS_DIRECTORY/\$TIMESTAMPED_NAME
 
 if [ ! -d \"$MOUNT_LOCATION/$APP_NAME\" ]; then
-  sudo mkdir -p $MOUNT_LOCATION/$APP_NAME
+  mkdir -p $MOUNT_LOCATION/$APP_NAME
 fi
 
 # Unmount the old one
-if [ ! -d \"$MOUNT_LOCATION/$APP_NAME\" ]; then
-  sudo umount $MOUNT_LOCATION/$APP_NAME
-fi
+# if [ -d \"$MOUNT_LOCATION/$APP_NAME\" ]; then
+#   umount $MOUNT_LOCATION/$APP_NAME
+# fi
 
 # Mount the new one!
 sudo mount $FS_DIRECTORY/\$TIMESTAMPED_NAME $MOUNT_LOCATION/$APP_NAME -t squashfs -o loop
+
+# Clean up
+rm -Rf \$TMP_GIT_CLONE
 "
 
-echo "$STR" > hooks/post-commit
-chmod +x hooks/post-commit
+echo "$STR" > hooks/post-receive
+chmod +x hooks/post-receive
