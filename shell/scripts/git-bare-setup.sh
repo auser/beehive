90e8a61a48b1af6ae126d6d4ccfc17a1167697be
Ari Lerner <arilerner@mac.com>
2009-09-10 02:19:58 -0700

    Added entries back into the fstab

--- shell/scripts/git-bare-setup.sh
+++ shell/scripts/git-bare-setup.sh
@@ -23,9 +23,6 @@ SQUASH_BASE=$PREFIX/squashed_fs
 SRC_BASE=$PREFIX/src
 TMP_DIR=$PREFIX/tmp
 
-FS_DIRECTORY=$SQUASH_BASE/$APP_NAME
-MOUNT_LOCATION=$MOUNT_BASE/$APP_NAME
-
 cd `dirname $APP_NAME`
 
 if [ ! -d $REPOS_BASE/$APP_NAME ]; then
@@ -35,15 +32,6 @@ fi
 if [ ! -d $REPOS_BASE/$APP_NAME.git ]; then
   echo "cd $REPOS_BASE/$APP_NAME && git --bare init"
   cd $REPOS_BASE/$APP_NAME && git --bare init
-fi
-if [ ! -d $FS_DIRECTORY ]; then
-  mkdir -p $FS_DIRECTORY
-fi
-if [ ! -d $MOUNT_LOCATION ]; then
-  mkdir -p $MOUNT_LOCATION
-fi
-if [ ! -d $MOUNT_LOCATION ]; then
-  mkdir -p $MOUNT_LOCATION
 fi
 
 echo "Making post-commit hook"
@@ -58,7 +46,18 @@ DATE=\$(date +%s)
 TIMESTAMPED_NAME=$APP_NAME-\$DATE.sqsh
 GIT_REPOS=\$(pwd)
 TMP_GIT_CLONE=$TMP_DIR/$APP_NAME
+FS_DIRECTORY=$SQUASH_BASE/$APP_NAME
+
 MOUNT_FILE=$FS_DIRECTORY/$APP_NAME.sqsh
+MOUNT_LOCATION=$MOUNT_BASE/$APP_NAME
+
+if [ ! -d $MOUNT_LOCATION ]; then
+  mkdir -p $MOUNT_LOCATION
+fi
+
+if [ ! -d $FS_DIRECTORY ]; then
+  mkdir -p $FS_DIRECTORY
+fi
 
 # Checkout the git repos
 git clone \$GIT_REPOS \$TMP_GIT_CLONE
@@ -70,7 +69,13 @@ mksquashfs \$TMP_GIT_CLONE $FS_DIRECTORY/\$TIMESTAMPED_NAME
 ln -sf $FS_DIRECTORY/\$TIMESTAMPED_NAME \$MOUNT_FILE
 
 # Get it ready to mount
-# Unmount the old ones
+
+# Make sure it's in the fstab so it will auto mount on reboot
+if [ \$( grep \"$APP_NAME\" /etc/fstab | wc -l) -eq 0 ]; then
+  echo \"\$MOUNT_FILE $MOUNT_LOCATION squashfs  ro,users,auto,nohide 0 0\" >> /etc/fstab
+fi
+
+# Unmount the old one
 if [ ! \$(mount | grep -q $APP_NAME) ]; then
   for i in \$(mount | grep $APP_NAME | awk '{print \$1}'); do
     echo 'unmounting \$i'
