7b18bd489721c2ff59c413585e994801f86f83d5
Ari Lerner <arilerner@mac.com>
2009-09-10 15:34:26 -0700

    Changing structure

--- erlang/src/discovery/app_discovery.erl
+++
@@ -1,5 +0,0 @@
--module (app_discovery).
--export ([discover_local_apps/0]).
-
-discover_local_apps() ->
-  [].
--- ruby/beehive/lib/beehive.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.dirname(__FILE__)+"/beehive")
-
-%w(askable colors).each do |lib|
-  require File.dirname(__FILE__)+"/beehive/mixins/#{lib}"
-end
-
-module Beehive
-end
