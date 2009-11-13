6f0454851029a550766a5f2194431b13d774a8ab
Ari Lerner <arilerner@mac.com>
2009-11-12 18:24:22 -0800

    Added basic clouds.rb template for starting on PoolParty

---
+++ config/clouds.rb
@@ -0,0 +1,15 @@
+pool "beehive" do
+  
+  cloud "router" do
+    using :ec2
+    instances 1
+    image_id "ami-1515f67c"
+    user 'ubuntu'
+    security_group do
+      authorize :from_port => 22, :to_port => 22
+      authorize :from_port => 8080, :to_port => 8080
+    end
+    user_data open("#{File.dirname(__FILE__)}/user-data/router.sh").read
+  end
+  
+end
---
+++ config/user-data/router.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd /tmp
+
+sudo apt-get update
+sudo apt-get install -y build-essential m4 libssl-dev libncurses5 libncurses5-dev
+sudo apt-get install -y erlang
+sudo apt-get autoremove -y
+
+wget http://www.erlang.org/download/otp_src_R13B02-1.tar.gz
+tar -zxf otp_src_R13B02-1.tar.gz
+cd otp_src_R13B02-1
+./configure  –enable-smp-support –enable-threads –enable-kernel-poll –enable-hipe  -with-ssl
+make
