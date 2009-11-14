b6e7984c8c63fcc03b0a5415a6ff460db7db48f0
Ari Lerner <arilerner@mac.com>
2009-11-14 03:12:24 -0800

    Updated app controller listing

--- lib/erlang/src/router/app_controllers/app_controller.erl
+++ lib/erlang/src/router/app_controllers/app_controller.erl
@@ -7,8 +7,30 @@
 %%%-------------------------------------------------------------------
 
 -module (app_controller).
-
+-include ("router.hrl").
 -export ([get/1, post/2, put/2, delete/2]).
+
+get(["all"]) -> 
+  All = app:all(),
+  [
+    "<h2>All Apps</h2>",
+    "<table>\n",
+    "<tr> ",
+    [["<td><b>", X, "</b></td>"] || X <- [
+      "Name", "Url", "LastUpdated"
+    ]],
+    "\n",
+    lists:map(fun(App) ->
+      [
+        "<tr>",
+        io_lib:format("<td> ~s </td>", [App#app.name]),
+        io_lib:format("<td> ~s </td>", [App#app.url]),
+        io_lib:format("<td> ~s </td>", [date_util:fmt_date(App#app.updated_at)]),
+        "</tr>"
+      ]
+    end, All),
+    "</table>\n"
+  ];
 
 get(_) -> "hello world".
 
