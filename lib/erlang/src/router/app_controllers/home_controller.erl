8bce0e8298808f0b515bc6ff1420780c8074a68f
Ari Lerner <arilerner@mac.com>
2009-11-15 03:53:51 -0800

    Added quick links to the homepage

--- lib/erlang/src/router/app_controllers/home_controller.erl
+++ lib/erlang/src/router/app_controllers/home_controller.erl
@@ -10,7 +10,18 @@
 
 -export ([get/1, post/2, put/2, delete/2]).
 
-get(_) -> "hello world".
+get(_) -> 
+  [
+    "<h3>Beehive</h3>",
+    "<table id='table'>",
+    "<tr>",
+    "<td><a href='/node'>Nodes</a></td>",
+    "<td><a href='/app'>Applications</a></td>",
+    "<td><a href='/stats'>Stats</a></td>",
+    "<td><a href='/backend'>Backend</a></td>",
+    "</tr>",
+    "</table>"
+  ].
 
 post(_Path, _Data) -> "unhandled".
 put(_Path, _Data) -> "unhandled".
