3ab857f478303ca3075cb4c4a857fbb8cd60a24b
Ari Lerner <arilerner@mac.com>
2009-11-19 14:10:04 -0800

    Added From into the port_handler for responses to the port_handler from the caller

--- lib/erlang/src/utils/port_handler.erl
+++ lib/erlang/src/utils/port_handler.erl
@@ -1,19 +1,19 @@
 -module (port_handler).
 -include ("common.hrl").
 -export([
-          start/2
+          start/3
         ]).
 
 % P = port_handler:start("thin -R beehive.ru --port 5000 start", "/Users/auser/Development/erlang/mine/router/test/fixtures/apps").
 
-start(Cmd, WorkingDir) ->
+start(Cmd, WorkingDir, From) ->
   process_flag(trap_exit, true),
   spawn_link(fun() ->
     Port = open_port({spawn, Cmd}, [{packet, 4}, exit_status, nouse_stdio, {cd, WorkingDir}]),
-    port_loop(Port, Cmd, WorkingDir)
+    port_loop(Port, Cmd, WorkingDir, From)
   end).
 
-port_loop(Port, Cmd, WorkingDir) ->
+port_loop(Port, Cmd, WorkingDir, From) ->
   receive
 	  {stop, StopCommand} -> 
 	    io:format("Stopping port: ~p~n", [StopCommand]),
@@ -23,12 +23,13 @@ port_loop(Port, Cmd, WorkingDir) ->
 	  {'EXIT',Port,_Reason} -> 
 	    ok;
 	  {Port, {exit_status, 0}} -> 
-	    io:format("Port decided to exit for sane reasons~n");
+	    io:format("Port decided to exit for sane reasons~n"),
+	    From ! {port_closed, Port};
       % port_close(Port);
     {Port, {exit_status, Code}} ->
 	    io:format("Port decided to exit for some reason: ~p", [Code]);
       % port_close(Port);
 	  Else ->
 	    io:format("Port received: ~p~n", [Else]),
-	    port_loop(Port, Cmd, WorkingDir)
+	    port_loop(Port, Cmd, WorkingDir, From)
   end.
