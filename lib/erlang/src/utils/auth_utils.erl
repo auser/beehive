ea7ef6a28c784b822490d8140b5b1f23d2b51c82
Ari Lerner <arilerner@mac.com>
2009-11-29 23:36:09 -0800

    Changed is_authorized_request/1 to get_authorized_user/1 in auth_utils

--- lib/erlang/src/utils/auth_utils.erl
+++ lib/erlang/src/utils/auth_utils.erl
@@ -11,13 +11,13 @@
 -compile(export_all).
 
 % Only run if there is a user associated with given token
-is_authorized_request(Data) ->
+get_authorized_user(Data) ->
   case proplists:is_defined(token, Data) of
     false -> false;
     true ->
       Token = proplists:get_value(token, Data),
       case users:find_by_token(Token) of
         [] -> false;
-        _ -> true
+        _ -> User
       end
   end.
