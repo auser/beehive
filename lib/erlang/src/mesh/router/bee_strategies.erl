8310eef3911b507f3cada01e63d8c5757bfc27da
Ari Lerner <arilerner@mac.com>
2009-12-01 18:24:47 -0800

    Added least_loaded/1 strategy in bee_strategies

--- lib/erlang/src/mesh/router/bee_strategies.erl
+++ lib/erlang/src/mesh/router/bee_strategies.erl
@@ -8,11 +8,28 @@
 %%%-------------------------------------------------------------------
 
 -module(bee_strategies).
+-include ("beehive.hrl").
 
--export([random/1]).
+-export([
+  random/1,
+  least_loaded/1
+]).
 
 random([]) -> [] ; %% Come back and update this,
                    %% throw an exception when emtpy?
 random( Backends ) ->
   RandNum = random:uniform(length(Backends)),
   lists:nth(RandNum, Backends).
+
+least_loaded(Backends) ->
+  ListsOfBees = lists:map(fun(B) ->
+    #bee_stat{current = CurrentReq} = case stats_srv:bee_dump(B#bee.id) of
+      [{_Name, Q}|_] -> Q;
+      _ -> stats_srv:new_bee_stat()
+    end,
+    {CurrentReq, B}
+  end, Backends),
+  SortedBees = lists:sort(fun({A, _},{B, _}) -> A < B end, ListsOfBees),
+  {_, Bee} = hd(SortedBees),
+  Bee.
+  
