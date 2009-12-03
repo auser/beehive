d1336a3efa7f7239cecacb1b72fcf279a4df17e8
Ari Lerner <arilerner@mac.com>
2009-12-03 01:25:41 -0800

    Added git_bh_srv

--- lib/erlang/src/mesh/storage/bh_git_srv.erl
+++ lib/erlang/src/mesh/storage/bh_git_srv.erl
@@ -2,7 +2,7 @@
 %%% File    : bh_git_srv.erl
 %%% Author  : Ari Lerner
 %%% Description : 
-%%%   Based on mojombo's egitd
+%%%   Based on Tom Preston-Werner's egitd (http://github.com/mojombo/egitd)
 %%% Created :  Wed Dec  2 20:02:41 PST 2009
 %%%-------------------------------------------------------------------
 
@@ -10,42 +10,48 @@
 -include ("beehive.hrl").
 -include ("common.hrl").
 
--export([start_link/0, init/1]).
+-export([
+  start_link/0, 
+  init/0, 
+  init/1,
+  init_accept/2
+]).
 
-start_link() ->
-  proc_lib:start_link(?MODULE, init, [self()]).
+start_link()          -> init().
+% Start listening on the application port
+init()                -> init(config:search_for_application_value(git_port, 9148, storage)).
+init(LocalPort) -> 
+  Pid = spawn_link(?MODULE, init_accept, [LocalPort, 10]),
+  {ok, Pid}.
 
-init(Parent) ->
-  Port = config:search_for_application_value(git_port, 9148, storage),
-  LSock = try_listen(Port, 10),
-  proc_lib:init_ack(Parent, {ok, self()}),
-  loop(LSock).
-    
-try_listen(Port, 0) ->
-  ?LOG(error, "Could not listen on the git port: ~p", [Port]);
-try_listen(Port, Times) ->
-  Res = gen_tcp:listen(Port, [list, {packet, 0}, {active, false}]),
-  case Res of
-    {ok, LSock} ->
-      ?LOG(info, "[git] Listening on port: ~p", [Port]),
-      LSock;
-    {error, _Reason} ->
-      timer:sleep(5000),
-      try_listen(Port, Times - 1)
+% accept responses on the port given by the application configuration
+init_accept(LPort, 0) -> ?LOG(error, "Could not listen on the git port: ~p", [LPort]);
+init_accept(LPort, Count) ->
+  SockOpts = [list, {packet, 0}, {active, false}],
+	case gen_tcp:listen(LPort, SockOpts) of
+	  {ok, ListenSocket} -> 
+	    accept(ListenSocket);
+	  Error ->
+	    ?LOG(error, "There was an error listening to the socket for port ~p: ~p", [LPort, Error]),
+	    init_accept(LPort, Count - 1)
+	end.
+
+accept(LSock) ->
+  case gen_tcp:accept(LSock) of
+    {ok, ClientSock} ->
+      spawn(fun() -> handle_method(ClientSock) end),
+	    accept(LSock);
+    Error ->
+      ?LOG(error, "There was an error accepting the socket ~p: ~p", [LSock, Error]),
+      exit(error)
   end.
-    
-loop(LSock) ->
-  {ok, Sock} = gen_tcp:accept(LSock),
-  spawn(fun() -> handle_method(Sock) end),
-  loop(LSock).
-  
+
 handle_method(Sock) ->
   % get the requested host and method
   case gen_tcp:recv(Sock, 0) of
     {ok, Header} ->
       {ok, Host} = extract_host(Header),
       Method = extract_method_name(Header),
-  
       % dispatch
       handle_method_dispatch(Method, Sock, Host, Header);
     {error, closed} ->
