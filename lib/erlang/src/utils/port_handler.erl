8c6a3e80bf10a02b9c1e388c6cefdf5f3422c287
Ari Lerner <arilerner@mac.com>
2009-12-03 23:55:40 -0800

    Made port_handler more durable to follow along port processes

--- lib/erlang/src/utils/port_handler.erl
+++ lib/erlang/src/utils/port_handler.erl
@@ -1,36 +1,37 @@
 -module (port_handler).
 -include ("common.hrl").
 -export([
-          start/3
+          start/4
         ]).
 
 % P = port_handler:start("thin -R beehive.ru --port 5000 start", "/Users/auser/Development/erlang/mine/router/test/fixtures/apps").
 
-start(Cmd, WorkingDir, From) ->
+start(Cmd, WorkingDir, From, Opts) ->
   process_flag(trap_exit, true),
   spawn_link(fun() ->
-    ?LOG(info, "Starting in ~p", [file_utils:relative_path(WorkingDir)]),
-    Port = open_port({spawn, Cmd}, [{packet, 4}, exit_status, nouse_stdio, {cd, file_utils:relative_path(WorkingDir)}]),
+    Port = open_port({spawn, Cmd}, [exit_status, {cd, file_utils:relative_path(WorkingDir)}|Opts]),
     port_loop(Port, Cmd, WorkingDir, From)
   end).
 
 port_loop(Port, Cmd, WorkingDir, From) ->
   receive
+    {Port, {data, Data}} ->
+      From ! {data, Data},
+      port_loop(Port, Cmd, WorkingDir, From);
 	  {stop, StopCommand} -> 
-	    io:format("Stopping port: ~p~n", [StopCommand]),
 	    _ClosePort = open_port({spawn, StopCommand}, [{packet, 4}, in, {cd, WorkingDir}]),
       % port_close(Port), port_close(ClosePort),
 	    ok;
 	  {'EXIT',Port,_Reason} -> 
 	    ok;
 	  {Port, {exit_status, 0}} -> 
-	    io:format("Port decided to exit for sane reasons~n"),
-	    From ! {port_closed, self()};
-      % port_close(Port);
+      % io:format("Port decided to exit for sane reasons~n"),
+      % (catch port_close(Port)),
+	    From ! {port_closed, 0};
     {Port, {exit_status, Code}} ->
-	    io:format("Port decided to exit for some reason: ~p~n", [Code]);
-      % port_close(Port);
+      % io:format("Port decided to exit for some reason: ~p~n", [Code]),
+	    From ! {port_exited, Code};
 	  Else ->
-	    io:format("Port received: ~p~n", [Else]),
+	    io:format("Port unknowingly received: ~p~n", [Else]),
 	    port_loop(Port, Cmd, WorkingDir, From)
   end.
