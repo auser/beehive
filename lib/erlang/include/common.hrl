a0e7b978a24884212df2407929c7730d8709cc1d
Ari Lerner <arilerner@mac.com>
2009-12-03 23:58:53 -0800

    Added SHELL_SCRIPT reading to common.hrl

--- lib/erlang/include/common.hrl
+++ lib/erlang/include/common.hrl
@@ -14,3 +14,11 @@ end).
 -define (FIXTURES_DIR, file_utils:relative_path("test/fixtures")).
 % Figure this out... Hm
 -define (CONFIGS_DIR, file_utils:relative_path("webapp/config")).
+-define (SHELL_SCRIPTS_DIR, file_utils:relative_path("./../shell")).
+
+-define (SHELL_SCRIPT_PATH (Name), filename:join([?SHELL_SCRIPTS_DIR, lists:append([Name, ".sh"])])).
+-define (SHELL_SCRIPT (Name), fun() ->
+  {ok, Binary} = file:read_file(?SHELL_SCRIPT_PATH(Name)),
+  erlang:binary_to_list(Binary)
+ end()
+).
