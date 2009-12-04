9b8e307f6fb46fac6d795aa2fa89acaf1aa940a6
Ari Lerner <arilerner@mac.com>
2009-12-03 23:55:19 -0800

    Moved template_command_string to string_utils

--- lib/erlang/src/utils/string_utils.erl
+++ lib/erlang/src/utils/string_utils.erl
@@ -1,7 +1,11 @@
 -module (string_utils).
 -author("Serge: onerlang.blogspot.com").
 -purpose("string replace functionality").
+
 -export([sub/3,gsub/3,test/0]).
+
+% Mine
+-export ([template_command_string/2]).
 -import (string, [len/1, str/2, left/2,right/2,concat/2]).
 
 sub(Str,Old,New) ->
@@ -26,6 +30,16 @@ subst(Str,_Old,_New, Str) -> Str;
 subst(Acc, Old, New,_Str) ->
          Acc1 = sub(Acc,Old,New),
          subst(Acc1,Old,New,Acc).
+
+% turn the command string from the comand string with the values
+% of [[KEY]] replaced by the corresponding proplist element of
+% the format:
+%   {[[PORT]], "80"}
+template_command_string(OriginalCommand, []) -> OriginalCommand;
+template_command_string(OriginalCommand, [{Str, Replace}|T]) ->
+  NewCommand = string_utils:gsub(OriginalCommand, Str, Replace),
+  template_command_string(NewCommand, T).
+
 
 test() ->
    io:format("~p ~p ~p ~p ~p ~p ~p ~n",
