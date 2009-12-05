adb525e71e982b7c9ea17cbe3e4435c5a30572a5
Ari Lerner <arilerner@mac.com>
2009-12-04 18:56:11 -0800

    Fixed output for pull-git-repos.sh

--- lib/shell/pull-git-repos.sh
+++ lib/shell/pull-git-repos.sh
@@ -8,9 +8,4 @@ mkdir -p [[DESTINATION]]
 git clone --depth 0 [[GIT_REPOS]] [[DESTINATION]] >/dev/null 2>&1
 cd [[DESTINATION]]
 SHA=$(git log --max-count=1 | awk '/commit/ {print $2}')
-
-if [ -z $SHA ]; then
-	exit 1
-else
-	echo "$SHA"
-fi
+echo "sha $SHA"
