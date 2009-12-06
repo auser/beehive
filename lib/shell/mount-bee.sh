976b30cc5bd451044c980a38b124fd7d167609a9
Ari Lerner <arilerner@mac.com>
2009-12-06 15:40:54 -0800

    Added new mount-bee.sh

--- lib/shell/mount-bee.sh
+++ lib/shell/mount-bee.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+PREFIX=/opt/beehive
+MOUNT_BASE=$PREFIX/mnt
+
 # mount a bee!
-OUT=$(hdiutil mount [[BEE_IMAGE]])
+APP_NAME=[[APP_NAME]]
+MOUNT_LOCATION=$MOUNT_BASE/$APP_NAME
+LOOP_DEVICE=/dev/$APP_NAME
+MOUNT_FILE=[[BEE_IMAGE]]
 
-read -r PATH VOL <<< $OUT
+echo "mount $MOUNT_FILE $MOUNT_LOCATION -t squashfs -o ro -o loop=$LOOP_DEVICE" > /tmp/out
+OUT=$(mount $MOUNT_FILE $MOUNT_LOCATION -t squashfs -o ro -o loop=$LOOP_DEVICE)
 
-echo "path $VOL"
+echo "path $MOUNT_LOCATION"
