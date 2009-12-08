c9a25ea54efe8fa57d9aed6190e869d2f687eb60
Ari Lerner <arilerner@mac.com>
2009-12-08 14:53:51 -0800

    Added echo repos_dir for pull-git-repos

--- lib/shell/pull-git-repos.sh
+++ lib/shell/pull-git-repos.sh
@@ -8,4 +8,6 @@ mkdir -p [[DESTINATION]]
 git clone --depth 0 [[GIT_REPOS]] [[DESTINATION]] >/dev/null 2>&1
 cd [[DESTINATION]]
 SHA=$(git log --max-count=1 | awk '/commit/ {print $2}')
+
+echo "repos_dir [[DESTINATION]]"
 echo "sha $SHA"
