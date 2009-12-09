d65783aff2e20c464e41dc1c5f07ead92213a049
Ari Lerner <arilerner@mac.com>
2009-12-08 17:53:37 -0800

    Added progress echos into create-bee as well as enforced the outfile location to be app_name/app_name.squashfs Mounting loop without the loop device specification in mount-bee

--- lib/shell/create-bee.sh
+++ lib/shell/create-bee.sh
@@ -2,11 +2,15 @@
 
 BASE_DIR=[[WORKING_DIRECTORY]]
 GIT_REPOS=[[LOCAL_GIT_REPOS]]
+SQUASHED_DIR=[[SQUASHED_DIRECTORY]]
 
+if [ ! -d [[WORKING_DIRECTORY]] ]; then
+	mkdir -p [[WORKING_DIRECTORY]]
+fi
 cd [[WORKING_DIRECTORY]]
 
 APP_NAME=[[APP_NAME]]
-OUTFILE=[[OUTFILE]]
+OUTFILE=$SQUASHED_DIR/$APP_NAME/$APP_NAME.squashfs
 
 APP_DIR=[[WORKING_DIRECTORY]]/$APP_NAME	
 GIT_REPOS_DIR=$APP_DIR/home/app
@@ -43,6 +47,7 @@ cd $GIT_REPOS_DIR
 SHA=$(git log --max-count=1 | awk '/commit/ {print $2}')
 echo "sha $SHA"
 cd ../
+echo "cloned $GIT_REPOS_DIR"
 
 # Clean the git repos of the pesky .git directory... 'cause who needs it anyway
 rm -rf $GIT_REPOS_DIR/.git
@@ -54,21 +59,31 @@ if [ -f $GEM_FILE ]; then
     gem install $line --no-ri --no-rdoc; 
   done
 fi
+echo "installed gems"
 
 # Set some fun environment variables here
 echo "
 SUPERMAN='a hero'
-" > $APP_DIR/.bashrc
+" > $APP_DIR/.profile
 
 
 # Make sure this directory doesn't exist
-rm $OUTFILE
+if [ -f $OUTFILE ]; then
+	mv $OUTFILE $OUTFILE.old
+fi
 
 # Make the squashfs filesystem
+T_DIR=$(dirname $OUTFILE)
+if [ ! -d $T_DIR ]; then
+	mkdir -p $T_DIR
+fi
+
 mksquashfs $APP_DIR $OUTFILE >/dev/null 2>&1
 
 if [ $? != 0 ]; then
 	exit 1
+else
+	echo "created_bee true"
 fi
 # mksquashfs $TMP_GIT_CLONE $FS_DIRECTORY/$TIMESTAMPED_NAME >/dev/null 2>&1
 # # Link it as the "latest" filesystem
@@ -82,3 +97,4 @@ echo "outdir $OUTFILE"
 
 # Cleanup
 rm -rf $APP_DIR
+echo "finished true"
--- lib/shell/mount-bee.sh
+++ lib/shell/mount-bee.sh
@@ -16,27 +16,26 @@ for i in $MOUNTED; do
 done
 sudo umount $LOOP_DEVICE >/dev/null 2>&1
 
-
 if [ ! -e $LOOP_DEVICE ]; then
   mknod -m 600 $LOOP_DEVICE b 7 0
 fi
 
-mkdir -p $MOUNT_LOCATION
+if [ ! -d $MOUNT_LOCATION ]; then
+	mkdir -p $MOUNT_LOCATION
+fi
 
 # Create chroot user
 CHROOT_USER="[[APP_NAME]]"
 
 if [ $(sudo cat /etc/passwd | grep $CHROOT_USER | grep -v "#" | wc -l) -eq 0 ]; then
   useradd -s /bin/sh -d $MOUNT_LOCATION/./ -c "$APP_NAME user" -g users $CHROOT_USER;
+	echo "added_user $CHROOT_USER"
 else
-  echo "";
+  echo "user_exists $CHROOT_USER";
 fi
 
-chown -R $CHROOT_USER $MOUNT_LOCATION
-chmod 700 $MOUNT_LOCATION
-
-# Mount it!
-mount $MOUNT_FILE $MOUNT_LOCATION -t squashfs -o ro -o loop=$LOOP_DEVICE
+# Mount it! loop=$LOOP_DEVICE
+mount $MOUNT_FILE $MOUNT_LOCATION -t squashfs -o ro -o loop
 
 # Bind mount the system
 mount --bind /bin $MOUNT_LOCATION/bin -o ro
@@ -46,6 +45,7 @@ mount --bind /lib $MOUNT_LOCATION/lib -o ro
 mount --bind /dev $MOUNT_LOCATION/dev -o ro
 mount -t proc /proc $MOUNT_LOCATION/proc
 mount --bind /tmp $MOUNT_LOCATION/tmp -o rw
+# Consider adding logs
 
 # If there is a lib64 directory 
 if [ -d /lib64 ]; then
@@ -61,3 +61,4 @@ sudo /usr/sbin/chroot $MOUNT_LOCATION /usr/bin/env -i \
 
 echo "app_name [[APP_NAME]]"
 echo "path $MOUNT_LOCATION"
+exit 0
