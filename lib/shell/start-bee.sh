411effc15cdaaa78e40508d6229a8c08cb49c858
Ari Lerner <arilerner@mac.com>
2009-12-11 13:22:48 -0800

    Added $THIN_APP in start-bee

--- lib/shell/start-bee.sh
+++ lib/shell/start-bee.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cd [[GIT_REPOS]]
+GEM_ENV=$(gem env | grep "EXECUTABLE DIRECTORY" | awk '{print $4}')
+THIN_APP="$GEM_ENV/thin"
+
+echo "thin $THIN_APP"
+cd [[APP_HOME]]
+$THIN_APP -R home/app/config.ru --log tmp/beehive.log --pid tmp/beehive-5001.pid --port [[PORT]] start
