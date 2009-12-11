a67327dfa5c4622ba66fd999732434134a2fa28e
Ari Lerner <arilerner@mac.com>
2009-12-10 18:29:26 -0800

    Added error messagine for slugger

--- lib/shell/pull-git-repos.sh
+++ lib/shell/pull-git-repos.sh
@@ -4,8 +4,9 @@
 # Shallow clone it
 # return the sha of the commit
 
+rm -rf [[DESTINATION]]
 mkdir -p [[DESTINATION]]
-git clone --depth 0 [[GIT_REPOS]] [[DESTINATION]] >/dev/null 2>&1
+git clone --depth 0 [[GIT_REPOS]] [[DESTINATION]] #>/dev/null 2>&1
 cd [[DESTINATION]]
 SHA=$(git log --max-count=1 | awk '/commit/ {print $2}')
 
