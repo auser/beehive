9cd956b84ea2d967e3610c742f5025891c8c85ad
Ari Lerner <arilerner@mac.com>
2009-12-11 15:22:42 -0800

    Updated the mesh util to update table copies around the area

--- lib/erlang/src/utils/mesh_util.erl
+++ lib/erlang/src/utils/mesh_util.erl
@@ -10,28 +10,42 @@
 
 -export([
          init_db_slave/1,
+         add_db_slave/2,
          get_random_pid/1
         ]).
 
 init_db_slave([]) -> ok; % safeguard
 init_db_slave(SeedNode) ->
-  case (catch(add_db_slave(SeedNode))) of
+  case (catch(add_db_slave(seed, SeedNode))) of
     {'EXIT', _} ->
       db:stop(),
       mnesia:delete_schema([node()]),
-      add_db_slave(SeedNode);
+      add_db_slave(seed, SeedNode);
     E -> 
       io:format("E: ~p~n", [E]),
       ok
   end.
 
 % Add an schema node
-add_db_slave(SeedNode) ->
-  db:start(),
-  mnesia:change_config(extra_db_nodes, [SeedNode]),
+% Add a slave node FROM the seed node
+add_db_slave(slave, SlaveNode) ->
+  case (catch mnesia:change_config(extra_db_nodes, [SlaveNode])) of
+    {error, _} -> clean_mnesia_from(SlaveNode);
+    _ -> ok
+  end,
   mnesia:change_table_copy_type(schema, node(), disc_copies),
   Tabs = mnesia:system_info(tables) -- [schema],
-  [mnesia:add_table_copy(Tab, node(), disc_copies) || Tab <- Tabs].
+  [mnesia:add_table_copy(Tab, SlaveNode, disc_copies) || Tab <- Tabs];
+% Add this slave node to the master at SeedNode
+add_db_slave(seed, SeedNode) ->
+  (catch db:start()),
+  mnesia:set_master_nodes([SeedNode]),
+  rpc:call(SeedNode, node_manager, add_slave_node, [node()]).
+
+clean_mnesia_from(SlaveNode) ->
+  rpc:call(SlaveNode, db, stop, []),
+  rpc:call(SlaveNode, mnesia, delete_schema, [[SlaveNode]]),
+  rpc:call(SlaveNode, db, start, []).
 
 get_random_pid(Name) ->
   L = case (catch pg2:get_members(Name)) of
