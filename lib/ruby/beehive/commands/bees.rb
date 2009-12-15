7dc17b8e68ba4c6e543870ad16d82c18ee2af680
David Guttman <davidguttman@aks-msi.(none)>
2009-12-14 16:20:23 -0800

    bees list

---
+++ lib/ruby/beehive/commands/bees.rb
@@ -0,0 +1,26 @@
+module Beehive
+  module Command
+    
+    class Bees < Base
+      
+      attr_reader :app_name
+      
+      def self.description
+        "List Bees"
+      end
+            
+      def run
+        parse_args do |opts|
+        end
+        
+        pp bees_list
+      end
+      
+      def bees_list
+        get("bees")
+      end
+            
+    end
+    
+  end
+end
