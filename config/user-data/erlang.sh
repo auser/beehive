d1c6d4494aa161d703fc2653cd329d5427f2ede8
Ari Lerner <arilerner@mac.com>
2009-12-16 16:16:34 -0800

    Moved the queen-install script to be called apiary

--- config/user-data/erlang.sh
+++ config/user-data/erlang.sh
@@ -5,8 +5,8 @@ ERL=$(which erl)
 cd /tmp
 if [ -z "$ERL" ]; then
 	wget http://www.erlang.org/download/otp_src_R13B03.tar.gz
-	tar -zxf otp_src_R13B02-1.tar.gz
-	cd otp_src_R13B02-1
+	tar -zxf otp_src_R13B03-1.tar.gz
+	cd otp_src_R13B03-t
 	./configure  --enable-smp-support --enable-threads --enable-kernel-poll --enable-hipe --with-ssl
 	make
 	sudo make install
