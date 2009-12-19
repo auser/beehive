baa0a0c0026e324b09751a59e3af3b96d8996a50
Ari Lerner <arilerner@mac.com>
2009-12-18 19:51:56 -0800

    Added apiary installation helps for the remote_install Created 256 loopback devices on the nodes (install) Added gitosis installation for the storage (install)

--- config/user-data/node.sh
+++ config/user-data/node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y build-essential m4 libssl-dev libncurses5 libncurses5-dev
 sudo apt-get install -y ruby rubygems ruby-dev libopenssl-ruby
 sudo apt-get install -y squashfs-tools
@@ -9,5 +9,10 @@ sudo apt-get autoremove -y
 # So we can deploy thin and rack apps
 sudo gem install rack thin --no-rdoc --no-ri
 sudo gem install haml sinatra --no-rdoc --no-ri
+
+# Create as many loop back devices as we can
+for i in $(seq 0 255); do
+	sudo mknod -m0660 /dev/loop$i b 7 $i 2>&1 >/dev/null
+done
 
 echo "HwlloE0lrd" > ~/.erlang.cookie
--- config/user-data/storage.sh
+++ config/user-data/storage.sh
@@ -3,9 +3,31 @@
 sudo apt-get update
 sudo apt-get install -y build-essential m4 libssl-dev libncurses5 libncurses5-dev
 sudo apt-get install -y git git-core
+sudo apt-get install -y rubygems1.8
 sudo apt-get install -y squashfs-tools
 sudo apt-get autoremove -y
 
 mkdir -p $PREFIX/squashed
 
 echo "HwlloE0lrd" > ~/.erlang.cookie
+
+# Install gitosis
+sudo apt-get install python-setuptools
+cd /tmp
+git clone git://eagain.net/gitosis.git 
+cd gitosis
+
+sudo python setup.py install
+
+sudo adduser --system --shell /bin/sh --gecos 'git version control' --group --disabled-password --home /home/git git
+sudo -H -u git mkdir /home/git/.ssh
+
+if [ -f /tmp/id_rsa.pub ]; then
+  sudo -H -u git gitosis-init < /tmp/id_rsa.pub
+  rm /tmp/id_rsa.pub
+else
+  ssh-keygen -f ~/.ssh/id_rsa -N "hello"
+  sudo -H -u git gitosis-init < ~/.ssh/id_rsa.pub
+fi
+
+sudo chmod 755 /home/git/repositories/gitosis-admin.git/hooks/post-update
