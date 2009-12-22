08fdcf1ed22cceda16ad40d4b617d196393834dc
fairchild <fairchild@stimble.net>
2009-12-22 12:45:16 -0800

    update router userdata to pull from the public github repo

--- config/user-data/router.sh
+++ config/user-data/router.sh
@@ -4,7 +4,7 @@ HOME=$BEEHIVE_USER_HOME
 INSTALL_PREFIX=${2:-''}
 
 sudo apt-get update
-sudo apt-get install -y curl
+sudo apt-get install -y curl git-core
 sudo apt-get install -y build-essential m4 libssl-dev libncurses5 libncurses5-dev
 sudo apt-get install -y ruby rubygems ruby-dev
 sudo apt-get install -y erlang-nox erlang-base-hipe erlang-dev erlang-tools
@@ -14,26 +14,18 @@ sudo mkdir -p $BEEHIVE_USER_HOME
 if [ $(sudo cat /etc/passwd | grep ^beehive | grep -v "#" | wc -l) -eq 0 ]; then
   useradd -s /bin/bash -b $BEEHIVE_USER_HOME -d $BEEHIVE_USER_HOME -c "beehive user" -g users beehive;
 fi
-sudo cp ~/.bashrc ~beehive/.bashrc
 echo "HwlloE0lrd" > $BEEHIVE_USER_HOME/.erlang.cookie
 sudo chmod 600 $BEEHIVE_USER_HOME/.erlang.cookie
 sudo chown beehive -R $BEEHIVE_USER_HOME
 
-####### behive stuff
-# mkdir -p $BEEHIVE_HOME/src && cd $BEEHIVE_HOME/src
-# git clone git@github.com:auser/beehive.git
-# curl -o $BEEHIVE_HOME/src/beehive.tgz https://github.com/auser/beehive/tarball/master
-
 cd /tmp
-curl -O https://cloudteam.s3.amazonaws.com/beehive.tgz 
-tar -zxvf beehive.tgz
-SRC_DIR="/tmp/beehive"
-cd $SRC_DIR/lib/erlang
+git clone --depth 0 git://github.com/auser/beehive.git
+cd beehive/lib/erlang
 make deps
 make
 make boot
 sudo make install
-chown -R beehive $INSTALL_PREFIX
-cd $SRC_DIR
 
+echo " starting beehive.. "
+$INSTALL_PREFIX/usr/bin/start_beehive -d -t router
 echo " -- completed router user-data script ---"
