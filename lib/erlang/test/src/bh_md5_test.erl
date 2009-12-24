ca02d9616ce27e55d5c7c3910f5fe846a4999fd7
Ari Lerner <arilerner@mac.com>
2009-12-24 14:38:11 -0800

    Renamed md5.erl to bh_md5 to separate from the installed libraries

--- lib/erlang/test/src/bh_md5_test.erl
+++ lib/erlang/test/src/bh_md5_test.erl
@@ -6,14 +6,14 @@
 %%% Created :  Thu Dec 17 13:12:13 PST 2009
 %%%-------------------------------------------------------------------
 
--module (md5_test).
+-module (bh_md5_test).
 
 -include_lib("eunit/include/eunit.hrl").
 
-md5_test_() ->
+bh_md5_test_() ->
   {inparallel,
     [
-      ?_assertEqual( md5:hex("hello world"), "5eb63bbbe01eeed093cb22bb8f5acdc3"),
-      ?_assertEqual(md5:hex(["hello world"]), md5:hex(["hello ", "world"]))
+      ?_assertEqual( bh_md5:hex("hello world"), "5eb63bbbe01eeed093cb22bb8f5acdc3"),
+      ?_assertEqual(bh_md5:hex(["hello world"]), bh_md5:hex(["hello ", "world"]))
     ]
   }.
