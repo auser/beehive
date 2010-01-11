3abe6acf4e83f7ac90bdd31c0ef71a6cc7e80042
Ari Lerner <arilerner@mac.com>
2010-01-11 07:58:06 -0800

    Updating starting proplists

--- lib/erlang/src/mesh/node/app_utils.erl
+++ lib/erlang/src/mesh/node/app_utils.erl
@@ -28,7 +28,7 @@ app_template_parsed(Type, Proplist, DefaultProps) ->
 merge_props([], _, Acc) -> Acc;
 merge_props([{K, _V}=Tuple|Rest], TemplateProps, Acc) ->
   NewAcc = case proplists:get_value(K, TemplateProps) of
-    undefined -> [{Tuple|Acc}];
+    undefined -> [Tuple|Acc}];
     Val -> [{K, Val}|Acc]
   end.
     
