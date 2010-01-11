f207da2cffcbae6baa9ea315fdc09a902c3a8e97
Ari Lerner <arilerner@mac.com>
2010-01-11 08:52:06 -0800

    Showing directory

--- lib/erlang/app_templates/rack.erl
+++ lib/erlang/app_templates/rack.erl
@@ -1,4 +1,4 @@
 % Example app template that will be used to start a bee
-{start_command, "ls -la && /usr/bin/thin -- -R home/app/config.ru --log tmp/[[APP_NAME]].log --port [[PORT]] start"}.
+{start_command, "ls && /usr/bin/thin -- -R home/app/config.ru --log tmp/[[APP_NAME]].log --port [[PORT]] start"}.
 {env, ["GEM_HOME=/home/.gems"]}.
 {dirs, ["/var/lib/gems/1.8", "/usr/bin", "/usr/lib/ruby/"]}.
