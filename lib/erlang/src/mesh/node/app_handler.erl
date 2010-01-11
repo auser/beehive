ccf198cdf4e37b80916084ddf578ae7568cbcb1c
Ari Lerner <arilerner@mac.com>
2010-01-11 08:51:08 -0800

    Added spacing in envs

--- lib/erlang/src/mesh/node/app_handler.erl
+++ lib/erlang/src/mesh/node/app_handler.erl
@@ -219,11 +219,11 @@ initialize_application(#app{template = Template} = App, PropLists, AppLauncher, 
   ],
   
   Env = lists:append([
-    [lists:flatten(["SHA=\"", Sha, "\""])],
-    [lists:flatten(["LOCAL_PORT=\"", misc_utils:to_list(Port), "\""])],
-    [lists:flatten(["LOCAL_HOST=\"", Host, "\""])],
-    [lists:flatten(["STARTED_AT=\"", misc_utils:to_list(StartedAt), "\""])],
-    [lists:flatten(["APP_NAME=\"", App#app.name, "\""])]
+    [lists:flatten([" SHA=\"", Sha, "\" "])],
+    [lists:flatten([" LOCAL_PORT=\"", misc_utils:to_list(Port), "\" "])],
+    [lists:flatten([" LOCAL_HOST=\"", Host, "\" "])],
+    [lists:flatten([" STARTED_AT=\"", misc_utils:to_list(StartedAt), "\" "])],
+    [lists:flatten([" APP_NAME=\"", App#app.name, "\" "])]
   ]),
   
   DefaultProps = [{env, Env}, {image, ImagePath}],
