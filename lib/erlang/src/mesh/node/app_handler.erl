d1019cea164f3f7f44bc0232bf2ea2de086af96a
Ari Lerner <arilerner@mac.com>
2010-01-11 14:43:29 -0800

    Typo in app_handler

--- lib/erlang/src/mesh/node/app_handler.erl
+++ lib/erlang/src/mesh/node/app_handler.erl
@@ -293,7 +293,7 @@ find_bee_on_storage_nodes(App, Sha, [Node|Rest]) ->
   end.
 
 % kill the instance of the application  
-internal_stop_instance(#bee{id = Id, os_pid = OsPid, pid = PidPort} = _CalledBee, App, From) when is_record(App, app) ->  
+internal_stop_instance(#bee{id = Id, os_pid = OsPid, pid = PidPort, port = Port, host = Host} = _CalledBee, App, From) when is_record(App, app) ->  
   #bee{commit_hash = Sha} = _Bee = bees:find_by_id(Id),
   ?LOG(debug, "internal_stop_instance: ~p and ~p", [Sha, App#app.name]),
   
@@ -307,7 +307,7 @@ internal_stop_instance(#bee{id = Id, os_pid = OsPid, pid = PidPort} = _CalledBee
   timer:sleep(100),
   babysitter:stop_process(PidPort),
   
-  case ets:lookup(?TAB_ID_TO_BEE, {App#app.name, Bee#bee.host, Bee#bee.port}) of
+  case ets:lookup(?TAB_ID_TO_BEE, {App#app.name, Host, Port}) of
     [{Key, _B}] ->
       ets:delete(?TAB_NAME_TO_BEE, App#app.name),
       ets:delete(?TAB_ID_TO_BEE, Key);
