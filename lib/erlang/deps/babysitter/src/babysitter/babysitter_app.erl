b7ef0f50b7d12d3a789ba2e1e7514860849bd65c
Ari Lerner <arilerner@mac.com>
2010-01-11 16:56:57 -0800

    exec

--- lib/erlang/deps/babysitter/src/babysitter/babysitter_app.erl
+++ lib/erlang/deps/babysitter/src/babysitter/babysitter_app.erl
@@ -16,7 +16,7 @@ start(_Type, _Args) ->
   %   io:format("Starting ~p...", [A]),
   %   A:start([])
   % end, [exec]),
-  exec:start_link([{alarm, Secs},{debug, true}]),
+  exec:start_link([{alarm, 20},{debug, true}]),
   babysitter_sup:start_link().
 
 stop(_State) -> 
