a176d3a272e2ed8cdbe523a4c9f0088664b9038b
Ari Lerner <arilerner@mac.com>
2010-04-26 13:15:19 -0700

    Removing old babysitter, preparing for new babysitter

--- lib/erlang/deps/babysitter/c_src/isolate/words.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cat "$@" | cpp -fpreprocessed | sed 's/[_a-zA-Z0-9][_a-zA-Z0-9]*/x/g' | tr -d ' \012' | wc -c
-
--- lib/erlang/deps/babysitter/test/src/test_suite.erl
+++
@@ -1,8 +0,0 @@
--module (test_suite).
-
--include_lib("eunit/include/eunit.hrl").
- 
-all_test_() ->
-  [
-    {module, babysitter_test}
-  ].
