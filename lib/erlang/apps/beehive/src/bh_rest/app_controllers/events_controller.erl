47366788138e785f7aa53a5c973b7319b834f423
Ari Lerner <arilerner@mac.com>
2010-06-11 16:11:04 -0700

    Added events_controller for latest_events listing

---
+++ lib/erlang/apps/beehive/src/bh_rest/app_controllers/events_controller.erl
@@ -0,0 +1,23 @@
+%%%-------------------------------------------------------------------
+%%% File    : events_controller.erl
+%%% Author  : Ari Lerner
+%%% Description : 
+%%%
+%%% Created :  Fri Nov 13 11:43:49 PST 2009
+%%%-------------------------------------------------------------------
+
+-module (events_controller).
+-include ("common.hrl").
+-include ("beehive.hrl").
+-include ("http.hrl").
+-export ([get/2, post/2, put/2, delete/2]).
+
+get(_, _Data) -> 
+  {events, dashboard_event_handler:get_latest_events()}.
+
+post(_Path, _Data) -> error("unhandled").
+put(_Path, _Data) -> error("unhandled").
+delete(_Path, _Data) -> error("unhandled").
+
+error(Msg) ->
+  {error, misc_utils:to_bin(Msg)}.
