d0736467cd4643f6edb8cd97a1001addde36108f
Ari Lerner <arilerner@mac.com>
2010-06-15 19:52:46 -0700

    Added router_srv tests Use rev-parse instead of git-log Starting basic rails app-template

---
+++ lib/erlang/apps/beehive_router/test/router_srv_tests.erl
@@ -0,0 +1,23 @@
+-module (router_srv_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+setup() ->
+  ok.
+  
+teardown(_X) ->
+  ok.
+
+starting_test_() ->
+  {spawn,
+    {setup,
+      fun setup/0,
+      fun teardown/1,
+      [
+        fun test_startup/0
+      ]
+    }
+  }.
+
+test_startup() ->
+  erlang:display({router_srv_tests, test_startup}),
+  passed.
