7189ce52dc49ce9ed9350e4e16aec2ef3cc2fa5b
Ari Lerner <arilerner@mac.com>
2010-07-22 11:54:01 -0700

    Updated start_dev

--- start_dev.sh
+++ start_dev.sh
@@ -5,22 +5,22 @@
 make compile
 
 # Make rood directory
-mkdir -p /tmp/beehive
-
 if [ -z $BEEHIVE_HOME ]; then
   BEEHIVE_HOME="/tmp/beehive"
 else
   BEEHIVE_HOME="$BEEHIVE_HOME"
 fi
 
+mkdir -p $BEEHIVE_HOME
+
 # Start Beehive
 echo "Starting beehive"
-exec "erl \
+erl \
     -name beehive@127.0.0.1 \
     -pa deps/*/ebin -pa lib/erlang/apps/*/ebin -pa lib/erlang/apps/*/include  \
-    -beehive database_dir '$BEEHIVE_HOME/db' \
-    -eval \"application:start(sasl)\" \
-    -eval \"application:start(os_mon)\" \
-    -eval \"application:start(crypto)\" \
+    -beehive database_dir '"$BEEHIVE_HOME/db"' \
+    -eval "application:start(sasl)" \
+    -eval "application:start(os_mon)" \
+    -eval "application:start(crypto)" \
 		-s reloader \
-    -eval \"application:start(beehive)\""
+    -eval "application:start(beehive)"
