9379bc3675cfecbbf749d8ffca50ca9d8152085d
Ari Lerner <arilerner@mac.com>
2010-07-26 13:14:46 -0700

    Force false with config search_for_application_value_from_config/1

--- lib/erlang/apps/beehive/src/bh_utils/config.erl
+++ lib/erlang/apps/beehive/src/bh_utils/config.erl
@@ -28,6 +28,7 @@ search_for_application_value_on_env(Param) ->
 
 search_for_application_value_from_config(Param) ->
 	case config:get(Param) of
+	  undefined -> false;
 		{error, _} -> false;
 		{ok, undefined} -> false;
 		{ok, V} -> V
