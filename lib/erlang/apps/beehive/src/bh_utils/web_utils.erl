640b5b17c9f182ea98304b8fb81a7e2843e06b83
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-08-11 13:30:20 -0700

    Killed beehive web_utils:query_params_to_proplist.  Wasn't getting used.

--- lib/erlang/apps/beehive/src/bh_utils/web_utils.erl
+++ lib/erlang/apps/beehive/src/bh_utils/web_utils.erl
@@ -2,7 +2,7 @@
 -include ("beehive.hrl").
 -include ("http.hrl").
 -compile (export_all).
-    
+
 % Private
 convert_to_struct(RawData) ->
   lists:map(fun({BinKey, BinVal}) ->
@@ -10,7 +10,7 @@ convert_to_struct(RawData) ->
       Val = misc_utils:to_list(BinVal),
       {Key, Val}
     end, RawData).
-    
+
 % jsonify the struct
 jsonify(JsonifiableBody) ->
   [ ?JSON_ENCODE({
@@ -25,14 +25,14 @@ to_json(V) -> json_encode(prepare_for_json(V)).
 prepare_for_json(Int) when is_integer(Int) -> Int;
 prepare_for_json(Float) when is_float(Float) -> Float;
 prepare_for_json(Atom) when is_atom(Atom) -> Atom;
-prepare_for_json(Array) when is_list(Array) -> 
+prepare_for_json(Array) when is_list(Array) ->
   case io_lib:printable_list(Array) of
     true ->
       erlang:list_to_binary(Array); % This is a string, not a list
     false ->
       list_to_json(Array, []) % This is a list, of some sort
   end;
-prepare_for_json(Tuple) when is_tuple(Tuple) -> 
+prepare_for_json(Tuple) when is_tuple(Tuple) ->
   tuple_to_json(Tuple, erlang:size(Tuple), []);
 prepare_for_json(V) -> V.
 
@@ -53,19 +53,7 @@ tuple_to_json(Tuple, CurrPos, Acc) ->
 
 json_encode(Value) -> mochijson2:encode(Value).
 
-% Turn query strings into proplists
-% String = token=hsdhfhdf&big=bear
-query_params_to_proplist(QueryString) ->
-  Strings = string:tokens(QueryString, "&"),
-  lists:flatten(lists:map(fun(Str) -> handle_single_query_key(Str, []) end, Strings)).
-
-handle_single_query_key([$=|Rest], Acc) -> handle_single_query_value(Rest, lists:reverse(Acc), []);
-handle_single_query_key([], _Acc) -> [];
-handle_single_query_key([Chr|Rest], Acc) -> handle_single_query_key(Rest, [Chr|Acc]).
-
-handle_single_query_value([], Key, Val) -> {misc_utils:to_atom(Key), misc_utils:to_list(lists:reverse(Val))};
-handle_single_query_value([Chr|Rest], K, Val) -> handle_single_query_value(Rest, K, [Chr|Val]).
-  
 ip_to_list({IP, _Port}) -> ip_to_list(IP);
-ip_to_list({A, B, C, D}) -> lists:flatten(io_lib:format("~w.~w.~w.~w", [A, B, C, D]));
+ip_to_list({A, B, C, D}) ->
+    lists:flatten(io_lib:format("~w.~w.~w.~w", [A, B, C, D]));
 ip_to_list(List) -> List.
--- lib/erlang/apps/beehive/test/beehive/rest_test.erl
+++ lib/erlang/apps/beehive/test/beehive/rest_test.erl
@@ -5,7 +5,7 @@ setup() ->
   rest_server:start_link(),
   timer:sleep(100),
   ok.
-  
+
 teardown(_X) ->
   ok.
 
@@ -15,52 +15,17 @@ starting_test_() ->
       fun setup/0,
       fun teardown/1,
       [
-        fun test_connectable/0,
-        fun test_apps/0
+        fun test_connectable/0
       ]
     }
   }.
 
 test_connectable() ->
-  {ok, ReturnedData} = fetch_url(get, [{host, "127.0.0.1"}, {port, 4999}, {path, "/apps.json"}, {headers, [{host, "beehive"}]}]),
-  Data = lists:flatten(ReturnedData),
-  ?assertEqual("HTTP/1.0 200 OK", string:sub_string(Data, 1, 15)),
+  {ok, Headers, Body} =
+        bh_test_util:fetch_url(get,
+                               [{host, "127.0.0.1"},
+                                {port, 4999},
+                                {path, "/apps.json"},
+                                {headers, [{host, "beehive"}]}]),
+  ?assertEqual("HTTP/1.0 200 OK", Headers),
   passed.
-
-test_apps() ->
-  {ok, ReturnedData} = fetch_url(get, [{host, "127.0.0.1"}, {port, 4999}, {path, "/apps.json"}, {headers, [{host, "beehive"}]}]),
-  Data = lists:flatten(ReturnedData),
-  ?assertEqual("HTTP/1.0 200 OK", string:sub_string(Data, 1, 15)),
-  passed.
-
-fetch_url(Method, Props) ->
-  Host    = proplists:get_value(host, Props, "localhost"),
-  Port    = proplists:get_value(port, Props, undefined),
-  Path    = proplists:get_value(path, Props, "/"),
-
-  Headers = proplists:get_value(headers, Props, []),
-  
-  {ok, Sock} = gen_tcp:connect(Host, Port, [list]),
-  
-  RequestLine = lists:flatten([string:to_upper(atom_to_list(Method)), " ", Path, " HTTP/1.0\r\n", 
-                lists:map(fun({Key, Value}) ->
-                  lists:flatten([string:to_upper(atom_to_list(Key)), ": ", Value, "\n"])
-                end, Headers), "\r\n"]),
-  gen_tcp:send(Sock, RequestLine),
-  proxy_loop(Sock, []).
-  
-proxy_loop(Sock, Acc) ->
-  receive
-	  {tcp, Sock, Data} ->
-      % Received data
-      proxy_loop(Sock, [Data|Acc]);
-    {tcp_closed, Sock} ->
-      {ok, lists:reverse(Acc)};
-  	{tcp_error, Sock} ->
-      {error, Sock};
-  	_Else -> proxy_loop(Sock, Acc)
-  % If there is no activity for a while and the socket has not already closed, 
-  % we'll assume that the connection is tired and should close, so we'll close it
-  after 1000 ->
-    {ok, lists:reverse(Acc)}
-  end.
