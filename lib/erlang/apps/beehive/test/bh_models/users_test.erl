4c1cdfa5bfcb4701836b3fbbff4ca75c4c1ef9e4
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-08-12 10:28:03 -0700

    I don't really like boilerplate.

--- lib/erlang/apps/beehive/test/bh_models/users_test.erl
+++ lib/erlang/apps/beehive/test/bh_models/users_test.erl
@@ -2,32 +2,13 @@
 -include ("beehive.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-setup() ->
-  ok.
-
-teardown(_X) ->
-  ok.
-
-starting_test_() ->
-  {inorder,
-   [{setup,
-     fun setup/0,
-     fun teardown/1,
-     [
-      fun create_hashes_password/0,
-      fun create_new_token/0
-     ]}
-   ]
-  }.
-
-
-create_hashes_password() ->
+create_hashes_password_test() ->
   {ok, User} = users:create(#user{email = "passtest@getbeehive.com",
                                   password = "atest"}),
   ?assertEqual(User#user.password, bh_md5:hex("atest")),
   passed.
 
-create_new_token() ->
+create_new_token_test() ->
   {ok, User} = users:create(#user{email = "token@getbeehive.com",
                                   password = "test"}),
   {ok, UserWithToken} = users:create_new_token_for(User),
