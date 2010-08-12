c401ccb14ce96878289e17d5fd371cf93979fb63
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-08-12 14:38:30 -0700

    Refactor. Push response_json into bh_test_util.

--- lib/erlang/apps/beehive/test/bh_rest/users_controller_test.erl
+++ lib/erlang/apps/beehive/test/bh_rest/users_controller_test.erl
@@ -27,7 +27,7 @@ get_index() ->
     bh_test_util:fetch_url(get,
                            [{path, "/users.json"}]),
   ?assertEqual("HTTP/1.0 200 OK", Header),
-  [Json|Rest] = response_json(Response),
+  [Json|Rest] = bh_test_util:response_json(Response),
   {"users", Users} = Json,
   ?assert(is_list(Users)),
   ?assert(lists:any(fun(E) -> 
@@ -37,19 +37,3 @@ get_index() ->
 
 get_index_with_email() ->
   passed.
-
-response_json(Response) ->
-  Json = lists:last(Response),
-  BodyStruct = mochijson2:decode(Json),
-  parse_json_struct(BodyStruct).
-
-parse_json_struct({struct, List}) ->  parse_json_struct(List);
-parse_json_struct({Key, Value}) ->
-  {binary_to_list(Key), parse_json_struct(Value) };
-parse_json_struct(List) when is_list(List) ->
-  lists:map( fun(E) -> 
-                 parse_json_struct(E)
-             end, List);
-parse_json_struct(Binary) when is_binary(Binary) ->  binary_to_list(Binary);
-parse_json_struct(Int) when is_integer(Int) -> Int.
-
--- lib/erlang/apps/beehive/test/bh_test_util.erl
+++ lib/erlang/apps/beehive/test/bh_test_util.erl
@@ -161,3 +161,19 @@ delete_all(Table) ->
     Pluralized:delete(O)
   end, Pluralized:all()).
   % beehive_db_srv:delete_all(Table).
+
+response_json(Response) ->
+  Json = lists:last(Response),
+  BodyStruct = mochijson2:decode(Json),
+  parse_json_struct(BodyStruct).
+
+parse_json_struct({struct, List}) ->  parse_json_struct(List);
+parse_json_struct({Key, Value}) ->
+  {binary_to_list(Key), parse_json_struct(Value) };
+parse_json_struct(List) when is_list(List) ->
+  lists:map( fun(E) ->
+                 parse_json_struct(E)
+             end, List);
+parse_json_struct(Binary) when is_binary(Binary) ->  binary_to_list(Binary);
+parse_json_struct(Int) when is_integer(Int) -> Int.
+
