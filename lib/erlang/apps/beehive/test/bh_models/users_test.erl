f73bb0c89de57211e86809334b295b459d9fb6c1
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-08-12 09:54:40 -0700

    Attempting to fix indentation.  Still trying to nail down editor settings.

--- lib/erlang/apps/beehive/test/bh_models/users_test.erl
+++ lib/erlang/apps/beehive/test/bh_models/users_test.erl
@@ -4,32 +4,32 @@
 
 setup() ->
   ok.
-  
+
 teardown(_X) ->
   ok.
 
 starting_test_() ->
   {inorder,
-    [{setup,
-        fun setup/0,
-        fun teardown/1,
-        [
-         fun create_hashes_password/0,
-         fun create_new_token/0
-        ]}
-    ]
+   [{setup,
+     fun setup/0,
+     fun teardown/1,
+     [
+      fun create_hashes_password/0,
+      fun create_new_token/0
+     ]}
+   ]
   }.
 
 
 create_hashes_password() ->
-    {ok, User} = users:create(#user{email = "passtest@getbeehive.com",
-                                    password = "atest"}),
-    ?assertEqual(User#user.password, bh_md5:hex("atest")),
-    passed.
+  {ok, User} = users:create(#user{email = "passtest@getbeehive.com",
+                                  password = "atest"}),
+  ?assertEqual(User#user.password, bh_md5:hex("atest")),
+  passed.
 
 create_new_token() ->
-    {ok, User} = users:create(#user{email = "token@getbeehive.com",
-                                    password = "test"}),
-    {ok, UserWithToken} = users:create_new_token_for(User),
-    ?assert(undefined =/= UserWithToken#user.token),
-    passed.
+  {ok, User} = users:create(#user{email = "token@getbeehive.com",
+                                  password = "test"}),
+  {ok, UserWithToken} = users:create_new_token_for(User),
+  ?assert(undefined =/= UserWithToken#user.token),
+  passed.
--- lib/erlang/apps/beehive/test/bh_rest/auth_controller_test.erl
+++ lib/erlang/apps/beehive/test/bh_rest/auth_controller_test.erl
@@ -12,44 +12,44 @@ teardown(_X) ->
 
 starting_test_() ->
   {inorder,
-    {setup,
-      fun setup/0,
-      fun teardown/1,
-      [
-        fun test_post/0,
-        fun test_post_wrong_email/0,
-        fun test_post_wrong_password/0
-      ]
-    }
+   {setup,
+    fun setup/0,
+    fun teardown/1,
+    [
+     fun test_post/0,
+     fun test_post_wrong_email/0,
+     fun test_post_wrong_password/0
+    ]
+   }
   }.
 
 test_post() ->
   {ok, Headers, Body} = post_to_auth([{email, "test@getbeehive.com"},
                                       {password, "test"}]),
-    ?assertEqual("HTTP/1.0 200 OK", Headers),
+  ?assertEqual("HTTP/1.0 200 OK", Headers),
   passed.
 
 
 test_post_wrong_email() ->
-    {ok, Headers, Body} = post_to_auth([{email, "noexist@getbeehive.com"},
-                                        {password, "test"}]),
-    ?assertEqual("HTTP/1.0 404 Object Not Found", Headers),
+  {ok, Headers, Body} = post_to_auth([{email, "noexist@getbeehive.com"},
+                                      {password, "test"}]),
+  ?assertEqual("HTTP/1.0 404 Object Not Found", Headers),
   passed.
 
 test_post_wrong_password() ->
-    {ok, Headers, Body} = post_to_auth([{email, "test@getbeehive.com"},
-                                        {password, "wrongpass"}] ),
-    ?assertEqual("HTTP/1.0 404 Object Not Found", Headers),
+  {ok, Headers, Body} = post_to_auth([{email, "test@getbeehive.com"},
+                                      {password, "wrongpass"}] ),
+  ?assertEqual("HTTP/1.0 404 Object Not Found", Headers),
   passed.
 
 post_to_auth(Params) ->
-    bh_test_util:fetch_url(post,
-                           [{host, "127.0.0.1"},
-                            {port, 4999},
-                            {path, "/auth.json"},
-                            {headers, [{"Content-Type",
-                                        "application/x-www-form-urlencoded" }]},
-                            {params, Params}
-                           ]
-                          ).
+  bh_test_util:fetch_url(post,
+                         [{host, "127.0.0.1"},
+                          {port, 4999},
+                          {path, "/auth.json"},
+                          {headers, [{"Content-Type",
+                                      "application/x-www-form-urlencoded" }]},
+                          {params, Params}
+                         ]
+                        ).
 
