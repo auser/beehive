32d8f8dcbc7556e8d417964c97984b1d833e0e9b
Ari Lerner <arilerner@mac.com>
2010-08-13 16:40:52 -0700

    Added check for on app:update/1 on the revision

--- lib/erlang/apps/beehive/src/bh_models/apps.erl
+++ lib/erlang/apps/beehive/src/bh_models/apps.erl
@@ -122,6 +122,10 @@ restart_by_name(Name) ->
 update([], _) -> ok;
 update(App, NewProps) when is_record(App, app) ->
   NewApp = misc_utils:update_proplist(to_proplist(App), NewProps),
+  case App#app.revision =/= NewProps#app.revision of
+    true -> ?NOTIFY({app, updated_revision, NewApp});
+    false -> ok
+  end,
   {ok, NewApp1} = save(NewApp),
   {updated, NewApp1};
 update(Name, NewProps) ->
