341d7ee5919d0460f4bc6fb69d345c4505924e95
Ari Lerner <arilerner@mac.com>
2010-08-16 11:53:03 -0700

    Changed the strings back to atoms in auth controller post/0

--- start_dev.sh
+++ start_dev.sh
@@ -1,26 +1,24 @@
-#!/usr/bin/env sh
+#!/bin/sh
 #cd `dirname $0`
 
 # Compile
 make compile
 
-# Make rood directory
-if [ -z $BEEHIVE_HOME ]; then
-  BEEHIVE_HOME="/tmp/beehive"
+# Make root directory
+if [ -z "$BEEHIVE_HOME" ]; then
+  mkdir -p /tmp/beehive
 else
-  BEEHIVE_HOME="$BEEHIVE_HOME"
+  echo "mkdir -p $BEEHIVE_HOME"
 fi
-
-mkdir -p $BEEHIVE_HOME
 
 # Start Beehive
 echo "Starting beehive"
-erl \
+eval "erl \
     -name beehive@127.0.0.1 \
     -pa deps/*/ebin -pa lib/erlang/apps/*/ebin -pa lib/erlang/apps/*/include  \
-    -beehive database_dir '"$BEEHIVE_HOME/db"' \
-    -eval "application:start(sasl)" \
-    -eval "application:start(os_mon)" \
-    -eval "application:start(crypto)" \
+    -beehive database_dir '\"$BEEHIVE_HOME/db\"' \
+    -eval \"application:start(sasl)\" \
+    -eval \"application:start(os_mon)\" \
+    -eval \"application:start(crypto)\" \
 		-s reloader \
-    -eval "application:start(beehive)"
+    -eval \"application:start(beehive)\""
