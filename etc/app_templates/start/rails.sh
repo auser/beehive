9d97aaf4a11b6f76241e68002b7ad4ef0f373db9
Ari Lerner <arilerner@mac.com>
2010-08-17 14:55:30 -0700

    Removed unmount command from the beehive_bee_object_config.conf (let cleanup do the work)

--- etc/app_templates/start/rails.sh
+++ etc/app_templates/start/rails.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
-echo "Starting rails '$NAME'"
+echo "Starting rails '$NAME' in $RUN_DIR"
 mkdir -p .beehive_gem_home
 export RAILS_ENV=production
-export GEM_HOME=.beehive_gem_home
-export GEM_PATH=.beehive_gem_home
-export PATH=$PATH:.beehive_gem_home/bin
+export GEM_HOME=$RUN_DIR/.beehive_gem_home
+export GEM_PATH=$RUN_DIR/.beehive_gem_home
+export PATH=$PATH:$RUN_DIR/.beehive_gem_home/bin
 
 echo $$ > $PIDFILE
 if [ -f start.sh ]; then
