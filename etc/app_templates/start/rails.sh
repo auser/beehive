235c4659d72075f175f9073be238f1e665b05c06
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-08-24 14:13:56 -0700

    isolate.now! works more better now

--- etc/app_templates/start/rails.sh
+++ etc/app_templates/start/rails.sh
@@ -6,12 +6,14 @@ export GEM_HOME=$RUN_DIR/.beehive_gem_home
 export GEM_PATH=$RUN_DIR/.beehive_gem_home:`gem env path`
 export PATH=$RUN_DIR/.beehive_gem_home/bin:$PATH
 
+
+echo $$ > $PIDFILE
+
 if [ -f "Isolate" ]; then
-  ruby -rubygems -e "require 'isolate'; Isolate.now!"
+  ruby -rubygems -e 'require "isolate"; Isolate.now!'
   $GEM_PATH
 fi
 
-echo $$ > $PIDFILE
 if [ -f start.sh ]; then
   echo "Using start.sh file"
   exec /bin/sh start.sh
