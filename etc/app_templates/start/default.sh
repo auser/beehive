35efa2074e5a43ec46a4c7a29bd005fe42482e9d
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-08-31 07:06:58 +0800

    May actually be causing some problems, but just wanted to get it committed.  deploy_env is storable and settable now, passed to start scripts as an arg.

--- etc/app_templates/start/default.sh
+++ etc/app_templates/start/default.sh
@@ -4,10 +4,11 @@ echo "Starting $NAME:$PORT"
 
 echo $$ > $PIDFILE
 
-echo "Starting in directory `pwd`"
+echo "Starting in directory `pwd` in $1 env"
 if [ -f start.sh ]; then
   echo "Using start.sh file"
   exec /bin/sh start.sh
 elif [ -f config.ru ]; then
+  export RACK_ENV=$1
   exec thin -R config.ru -p $PORT start
 fi
