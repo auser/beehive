49e78bb0f0b7821d65fff3bbe9f49fbe91ef9d5f
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-09-02 09:09:37 -0700

    Add -e param to script/server call

--- etc/app_templates/start/rails.sh
+++ etc/app_templates/start/rails.sh
@@ -6,7 +6,6 @@ export GEM_HOME=$RUN_DIR/.beehive_gem_home
 export GEM_PATH=$RUN_DIR/.beehive_gem_home:`gem env path`
 export PATH=$RUN_DIR/.beehive_gem_home/bin:$PATH
 
-
 echo $$ > $PIDFILE
 
 if [ -f "Isolate" ]; then
@@ -18,6 +17,6 @@ if [ -f start.sh ]; then
   echo "Using start.sh file"
   exec /bin/sh start.sh
 else
-  echo "./script/server -p $PORT"
-  exec ./script/server -p $PORT
+  echo "./script/server -p $PORT -e $RAILS_ENV"
+  exec ./script/server -p $PORT -e $RAILS_ENV
 fi
