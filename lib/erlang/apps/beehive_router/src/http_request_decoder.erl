d7869f6687dff5d3c9b8c35140e63b4954e973f7
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-09-29 07:56:03 -0700

    Support for routing apps based on subdirectory, rather than subdomain. Weak testing right now.  I need to figure out how to test logic that depends on app config settings.

--- lib/erlang/apps/beehive_router/src/http_request_decoder.erl
+++ lib/erlang/apps/beehive_router/src/http_request_decoder.erl
@@ -1,7 +1,7 @@
  %%%-------------------------------------------------------------------
 %%% File    : http_request_decoder.erl
 %%% Author  : Ari Lerner
-%%% Description : 
+%%% Description :
 %%%
 %%% Created :  Tue Nov 17 17:52:40 PST 2009
 %%%-------------------------------------------------------------------
@@ -12,49 +12,59 @@
 
 %% API
 -export([
-  handle_request/1
+         handle_request/1
 ]).
 %% TEST
 -export ([
-  parse_route_from_request/2
+          parse_route_from_request/2,
+          split_off_first_subdirectory/1
 ]).
 
 % Take the connecting socket and handle the request. Get the request up until the end of the headers
-% Take off the 'Host' parameter (or other sepcified parameter) from the header and return the 
+% Take off the 'Host' parameter (or other sepcified parameter) from the header and return the
 % routing key and the full request to the calling process
 handle_request(ClientSock) ->
   Req = beehive_request:new(ClientSock),
-  
+
   RoutingParameter = misc_utils:to_atom(config:search_for_application_value(routing_parameter, 'Host')),
-  HeaderVal = mochiweb_headers:get_value(RoutingParameter, Req:get(headers)),
-  
-  BaseDomain = config:search_for_application_value(domain, undefined),
-  Subdomain = parse_route_from_request(HeaderVal, BaseDomain),
-  ForwardReq = build_request_headers(Req),
+  case RoutingParameter of
+    'Host' ->
+      HeaderVal = mochiweb_headers:get_value(RoutingParameter, Req:get(headers)),
+      BaseDomain = config:search_for_application_value(domain, undefined),
+      Subdomain = parse_route_from_request(HeaderVal, BaseDomain),
+      ForwardReq = build_request_headers(Req);
+    subdirectory ->
+      [Subdomain|Path]  = split_off_first_subdirectory(Req:get(raw_path)),
+      ForwardReq = build_request_headers(Req, Path)
+  end,
   {ok, Subdomain, ForwardReq, Req}.
-  
+
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
-build_request_headers(Req) ->  
+build_request_headers(Req) ->
+  build_request_headers(Req, Req:get(raw_path)).
+build_request_headers(Req, ReqPath) ->
   {ok, Hostname} = inet:gethostname(),
-    
+
   Headers = mochiweb_headers:to_list(Req:get(headers)),
   Method = Req:get(method),
-  Path = Req:get(raw_path),
+  Path = ReqPath,
   Version = Req:get(version),
-  
-  NewHeaders = replace_values_in_prolists(
+
+  NewHeaders =
+    replace_values_in_prolists(
       [
-        {'X-Forwarded-For', Hostname}
+       {'X-Forwarded-For', Hostname}
       ],
       Headers),
 
   [
-    misc_utils:to_list(Method), " ", Path, " HTTP/", version(Version), <<"\r\n">> |
-    headers_to_list(NewHeaders)
+   misc_utils:to_list(Method), " ",
+   Path, " HTTP/", version(Version), <<"\r\n">> |
+   headers_to_list(NewHeaders)
   ].
-      
+
 % Replace values in a proplist
 replace_values_in_prolists(PropList, OriginalHeaders) ->
   lists:flatten(lists:map(fun({K,V}) ->
@@ -80,7 +90,7 @@ headers_to_list(Headers) ->
 %% @spec (Hostname, BaseDomain) ->    Domain
 %% @doc Parse the domain from the base domain
 %%  Take off the domain
-%%      
+%%
 %% @end
 %%-------------------------------------------------------------------
 parse_route_from_request(undefined, _) -> default;
@@ -89,27 +99,42 @@ parse_route_from_request(HostName, BaseDomain) ->
   [NoPortHostname|_] = string:tokens(HostName, ":"),
   O = string:tokens(NoPortHostname, "."),
   T = string:tokens(BaseDomain, "."),
-  
+
   case lists:subtract(O, T) of
     [] -> default; % The requested resource is no different from the basedomain
     List -> List
   end.
-  
+
 parse_route_from_request_without_base_domain(HostName) ->
   [NoPortHostname|_] = string:tokens(HostName, ":"),
   O = string:tokens(NoPortHostname, "."),
   parse_route_from_request_without_base_domain1(O, []).
 
 parse_route_from_request_without_base_domain1([], _Acc)     -> default;
-parse_route_from_request_without_base_domain1(["localhost"], Acc) ->  
+parse_route_from_request_without_base_domain1(["localhost"], Acc) ->
   parse_route_from_request_without_base_domain2(Acc);
-  
+
 parse_route_from_request_without_base_domain1(["com"], Acc) -> parse_route_from_request_without_base_domain2(Acc);
 parse_route_from_request_without_base_domain1(["org"], Acc) -> parse_route_from_request_without_base_domain2(Acc);
 parse_route_from_request_without_base_domain1(["net"], Acc) -> parse_route_from_request_without_base_domain2(Acc);
-parse_route_from_request_without_base_domain1([H|Rest], Acc) -> 
+parse_route_from_request_without_base_domain1([H|Rest], Acc) ->
   parse_route_from_request_without_base_domain1(Rest, [H|Acc]).
 
 parse_route_from_request_without_base_domain2([]) -> default;
 parse_route_from_request_without_base_domain2(List) ->
   [H|_Rest] = lists:reverse(List), [H].
+
+
+split_off_first_subdirectory("") -> [""|""];
+split_off_first_subdirectory(Path) ->
+  Split = string:tokens(Path, "/"),
+  [First|Rest] = Split,
+  [First|valid_path(Rest)].
+
+valid_path([]) -> "/";
+valid_path(E) -> join_path(E).
+
+join_path([]) -> "";
+join_path(Tokens) ->
+  [First|Rest] = Tokens,
+  lists:concat(["/", First,join_path(Rest)]).
--- lib/erlang/apps/beehive_router/test/http_request_decoder_tests.erl
+++ lib/erlang/apps/beehive_router/test/http_request_decoder_tests.erl
@@ -6,7 +6,7 @@ setup() ->
   ConfigFile = filename:join([Dir, "test", "fixtures", "beehive.cfg"]),
   application:set_env(beehive, config_file, ConfigFile),
   ok.
-  
+
 teardown(_X) ->
   ok.
 
@@ -16,14 +16,28 @@ starting_test_() ->
       fun setup/0,
       fun teardown/1,
       [
-        fun test_parse_route_from_request/0
+       fun test_parse_route_from_request/0,
+       fun test_split_off_first_subdir/0
       ]
     }
   }.
-  
+
 test_parse_route_from_request() ->
   ?assertEqual(["teeth"], http_request_decoder:parse_route_from_request("teeth.and.nails.test.beehive.com", undefined)),
   ?assertEqual(["hello"], http_request_decoder:parse_route_from_request("hello.test.beehive.com", "test.beehive.com")),
   ?assertEqual(["hello", "world"], http_request_decoder:parse_route_from_request("hello.world.test.beehive.com", "test.beehive.com")),
   ?assertEqual(default, http_request_decoder:parse_route_from_request("test.beehive.com", "test.beehive.com")),
+  passed.
+
+
+test_split_off_first_subdir() ->
+  Path = "/rservices/v1/getLocations?g=foo",
+  [Subdir| RestOfPath] =
+    http_request_decoder:split_off_first_subdirectory(Path),
+  ?assertEqual("rservices", Subdir),
+  ?assertEqual("/v1/getLocations?g=foo", RestOfPath),
+  ShortPath = "/syndication",
+  ["syndication"|RestOfShortPath] =
+    http_request_decoder:split_off_first_subdirectory(ShortPath),
+  ?assertEqual("/", RestOfShortPath),
   passed.
