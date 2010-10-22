3f30252ec02a97783793c9c03718940185141e9b
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-10-22 07:28:32 -0700

    Switching bootstrap back to check out auser's rack app fixture.

--- test/bootstrap.sh
+++ test/bootstrap.sh
@@ -2,5 +2,5 @@
 
 if [ ! -d test/fixtures/incredibly_simple_rack_app.git ]; then
   cd ./test/fixtures
-  git clone git://github.com/jdunphy/incredibly_simple_rack_app.git --mirror
+  git clone git://github.com/auser/incredibly_simple_rack_app.git --mirror
 fi
